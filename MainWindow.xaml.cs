fe09a73b4c9c03be22a13f3a748a7f2f8ad54c94
Pekka Alaluukas <pekka.alaluukas@oamk.fi>
2022-10-04 10:24:48 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace MySQLWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MySqlConnection conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["localMySql"].ConnectionString);
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("Select id_book, name, author,isbn from book", conn);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    tbResult.Text += reader.GetString(0) + " , " + reader.GetString(1) + " , " + reader.GetString(2) + "\n";
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally {
+                conn.Close();
+            }
+        }
+    }
+}
